3cd9242b344158eefdde4649fd0444e1afdeee3f
developer10160221 <developer10160221@gmail.com>
2019-08-01 22:29:35 +0900

    SpringBootDemo sources first commit

---
+++ src/main/java/com/example/demo/ApiController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class ApiController {
+    @GetMapping(path = "/helloWorld")
+    public String helloWorld() {
+        return LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    }
+}
